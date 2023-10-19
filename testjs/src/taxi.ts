670e0a365aaebcf744f79240a4a0228836e5d35a
Nong AnuchitO <anuchito.code@gmail.com>
2023-10-19 11:19:39 +0700

    able to calculate taxi fare

--- testjs/src/taxi.ts
+++ testjs/src/taxi.ts
@@ -1,1 +1,20 @@
 export const Taxi = () => 55
+
+export const calculateFare = (
+  distance: number,
+  waitingTime: number
+): number => {
+  return 2 * distance + waitingTime
+}
+
+export const roundDistance = (distance: number): number => {
+  return Math.ceil(distance * 2) / 2
+}
+
+export const roundWaitingTime = (waitingTime: number): number => {
+  return Math.ceil(waitingTime)
+}
+
+export const fare = (distance: number, waitingTime: number): number => {
+  return calculateFare(roundDistance(distance), roundWaitingTime(waitingTime))
+}
